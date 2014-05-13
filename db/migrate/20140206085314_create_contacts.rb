c32a5fccfe6aca0d5d07633d9e830adfc3b38970
iladiro <dirosa.ilaria@gmail.com>
2014-05-13 17:10:05 +0200

    pricing page

--- db/migrate/20140206085314_create_contacts.rb
+++ db/migrate/20140206085314_create_contacts.rb
@@ -1,10 +1,11 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
-      t.string :name
-      t.string :email
-      t.string :phone
-      t.text :message
+      t.string  :name
+      t.string  :email
+      t.string  :phone
+      t.text    :message
+      t.integer :newsletter, limit: 1
 
       t.timestamps
     end
