c05e696c3514bb8f32168a587f8dabfee77c9b83
biospank <fabio.petrucci@gmail.com>
2014-09-04 14:57:20 +0200

    fix on forem navigation links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,15 @@ module ApplicationHelper
   def nav_menu_item link_path, additional_class = nil, &block
 
     if link_path.is_a? String
-      class_name = [additional_class, (current_page?(link_path) ? 'active' : '')].join(' ')
+      if link_path != main_app.root_path
+        if request.path.start_with? link_path
+          class_name = [additional_class, 'active'].join(' ')
+        else
+          class_name = [additional_class, ''].join(' ')
+        end
+      else
+        class_name = [additional_class, (current_page?(link_path) ? 'active' : '')].join(' ')
+      end
     else
       class_name = [additional_class, ((link_path.any? {|link| current_page?(link)}) ? 'active' : '')].join(' ')
     end
