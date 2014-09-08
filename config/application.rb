295db9e8c100a5c8980870a5a9310010e25e958a
biospank <fabio.petrucci@gmail.com>
2014-09-08 10:56:46 +0200

    precompile emoji files

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,7 @@ module Paperclip
     end
 
     config.assets.paths << Emoji.images_path
+    config.assets.precompile << "emoji/**/*.png"
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
